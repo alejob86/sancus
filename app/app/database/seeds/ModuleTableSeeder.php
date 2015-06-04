d59ba87066371666716a59ffee41eaa25d0ca78e
alejob86 <alejandrobernalcollazos@gmail.com>
2015-06-04 00:13:38 -0300

    disabling sales and purchases in module table seeder for the moment

--- app/app/database/seeds/ModuleTableSeeder.php
+++ app/app/database/seeds/ModuleTableSeeder.php
@@ -35,6 +35,7 @@ class ModuleTableSeeder extends Seeder {
 							"name" => Config::get('constants.CLIENT_MODULE_NAME'),
 							"path" => Config::get('constants.CLIENT_MODULE_PATH')
 						),
+						/*
 						array(
 							"name" => Config::get('constants.SALE_MODULE_NAME'),
 							"path" => Config::get('constants.SALE_MODULE_PATH')
@@ -43,6 +44,7 @@ class ModuleTableSeeder extends Seeder {
 							"name" => Config::get('constants.PURCHASE_MODULE_NAME'),
 							"path" => Config::get('constants.PURCHASE_MODULE_PATH')
 						),
+						*/
 						array(
 							"name" => Config::get('constants.DOCTOR_MODULE_NAME'),
 							"path" => Config::get('constants.DOCTOR_MODULE_PATH')
